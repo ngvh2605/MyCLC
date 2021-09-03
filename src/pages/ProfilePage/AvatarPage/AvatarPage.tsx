5bc89b4157b186ff1eee67fc38e8c002cbbc2b90
Vũ Anh <vuanhlk12@gmail.com>
2021-09-03 21:16:46 +0700

    update resize img

--- src/pages/ProfilePage/AvatarPage/AvatarPage.tsx
+++ src/pages/ProfilePage/AvatarPage/AvatarPage.tsx
@@ -1,4 +1,4 @@
-import { Camera, CameraResultType, CameraSource } from "@capacitor/core";
+import { Camera, CameraResultType, CameraSource } from '@capacitor/core';
 import {
   IonAlert,
   IonAvatar,
@@ -19,45 +19,27 @@ import {
   IonTitle,
   IonToolbar,
   isPlatform,
-} from "@ionic/react";
-import { chevronBack } from "ionicons/icons";
-import React, { useEffect, useRef, useState } from "react";
-import { useHistory } from "react-router";
-import { useAuth } from "../../../auth";
-import { auth as firebaseAuth, database } from "../../../firebase";
-import useUploadFile from "../../../common/useUploadFile";
-import jimp from "jimp";
-
-async function resizeImage(url: string, callback) {
-  // Read the image.
-  const image = await jimp.read(url);
-
-  // Resize the image to width 150 and auto height.
-  console.log(image.getWidth(), image.getHeight());
-  const width = image.getWidth();
-  const height = image.getHeight();
-  if (width < height) {
-    await image.resize(jimp.AUTO, 400);
-  } else await image.resize(400, jimp.AUTO);
-  // Save and overwrite the image
-  //await image.writeAsync("test/image.png");
-  await image.getBase64Async(jimp.MIME_JPEG).then((newImage) => {
-    callback(newImage);
-  });
-}
+} from '@ionic/react';
+import { chevronBack } from 'ionicons/icons';
+import React, { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router';
+import { useAuth } from '../../../auth';
+import { auth as firebaseAuth, database } from '../../../firebase';
+import useUploadFile from '../../../common/useUploadFile';
+import { resizeImage } from '../../../utils/helpers/helpers';
 
 const AvatarPage: React.FC = () => {
   const { userId } = useAuth();
   const history = useHistory();
-  const [avatarUrl, setAvatarUrl] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState('');
   const [status, setStatus] = useState({ loading: false, error: false });
   const fileInputRef = useRef<HTMLInputElement>();
   const { progress, url, handleUpload } = useUploadFile(userId);
 
   const [isDisabled, setIsDisabled] = useState(true);
   const [showAlert, setShowAlert] = useState(false);
-  const [alertHeader, setAlertHeader] = useState("");
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alertHeader, setAlertHeader] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
 
   useEffect(() => {
     readData();
@@ -70,7 +52,7 @@ const AvatarPage: React.FC = () => {
 
   useEffect(
     () => () => {
-      if (avatarUrl.startsWith("blob:")) {
+      if (avatarUrl.startsWith('blob:')) {
         URL.revokeObjectURL(avatarUrl);
       }
     },
@@ -80,52 +62,55 @@ const AvatarPage: React.FC = () => {
   useEffect(() => {
     if (progress === 100) {
       setStatus({ loading: false, error: false });
-      setAlertHeader("Chúc mừng!");
-      setAlertMessage("Ảnh đại diện của bạn đã được cập nhật thành công");
+      setAlertHeader('Chúc mừng!');
+      setAlertMessage('Ảnh đại diện của bạn đã được cập nhật thành công');
       setShowAlert(true);
     }
   }, [progress]);
 
   const readData = () => {
-    const userData = database.ref().child("users").child(userId);
+    const userData = database.ref().child('users').child(userId);
 
-    userData.child("personal").on("value", (snapshot) => {
+    userData.child('personal').on('value', (snapshot) => {
       if (snapshot.exists()) {
         const data = snapshot.val();
         if (data.avatar) setAvatarUrl(data.avatar);
-        else setAvatarUrl("/assets/image/placeholder.png");
+        else setAvatarUrl('/assets/image/placeholder.png');
       } else {
-        console.log("No data available");
+        console.log('No data available');
       }
     });
   };
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     if (event.target.files.length > 0) {
       const file = event.target.files.item(0);
       const pictureUrl = URL.createObjectURL(file);
 
       setIsDisabled(false);
-      resizeImage(pictureUrl, function (result: string) {
-        setAvatarUrl(result);
-      });
+
+      const resizeUrl = await resizeImage(pictureUrl, 400);
+      setAvatarUrl(resizeUrl);
     }
   };
 
   const handlePictureClick = async () => {
-    if (isPlatform("capacitor")) {
+    if (isPlatform('capacitor')) {
       try {
         const photo = await Camera.getPhoto({
           resultType: CameraResultType.Uri,
           source: CameraSource.Prompt,
           width: 600,
         });
-        resizeImage(photo.webPath, function (result: string) {
-          setAvatarUrl(result);
-        });
+
+        const resizeUrl = await resizeImage(photo.webPath, 400);
+        setAvatarUrl(resizeUrl);
+
         setIsDisabled(false);
       } catch (error) {
-        console.log("Camera error:", error);
+        console.log('Camera error:', error);
       }
     } else {
       fileInputRef.current.click();
@@ -134,52 +119,52 @@ const AvatarPage: React.FC = () => {
 
   const handleUploadFile = async (url: string) => {
     setStatus({ loading: true, error: false });
-    await handleUpload(url, "avatar");
+    await handleUpload(url, 'avatar');
   };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton text="Huỷ" defaultHref="/my/profile" />
+          <IonButtons slot='start'>
+            <IonBackButton text='Huỷ' defaultHref='/my/profile' />
           </IonButtons>
           <IonTitle>Sửa ảnh đại diện</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
+      <IonContent className='ion-padding'>
         <IonAvatar
-          className="ion-margin"
+          className='ion-margin'
           style={{
             width: window.screen.height / 3,
             height: window.screen.height / 3,
-            marginLeft: "auto",
-            marginRight: "auto",
+            marginLeft: 'auto',
+            marginRight: 'auto',
           }}
         >
           <IonSkeletonText animated hidden={avatarUrl ? true : false} />
           <img
             hidden={avatarUrl ? false : true}
-            src={avatarUrl || "/assets/image/placeholder.png"}
-            alt=""
+            src={avatarUrl || '/assets/image/placeholder.png'}
+            alt=''
             onClick={handlePictureClick}
           />
         </IonAvatar>
         <input
-          type="file"
-          id="upload"
-          accept="image/*"
+          type='file'
+          id='upload'
+          accept='image/*'
           hidden
           multiple={false}
           ref={fileInputRef}
           onChange={handleFileChange}
         />
         <IonChip
-          color="primary"
-          style={{ height: "max-content", marginBottom: 10 }}
-          className="ion-margin"
+          color='primary'
+          style={{ height: 'max-content', marginBottom: 10 }}
+          className='ion-margin'
         >
-          <IonLabel text-wrap className="ion-padding">
+          <IonLabel text-wrap className='ion-padding'>
             Ấn vào khung ảnh để thay đổi. Nên chọn ảnh đại diện hình vuông hoặc
             đã được crop sẵn
           </IonLabel>
@@ -191,21 +176,21 @@ const AvatarPage: React.FC = () => {
           isOpen={showAlert}
           onDidDismiss={() => {
             setShowAlert(false);
-            history.replace("/my/profile");
+            history.replace('/my/profile');
           }}
-          cssClass="my-custom-class"
+          cssClass='my-custom-class'
           header={alertHeader}
           message={alertMessage}
-          buttons={["OK"]}
+          buttons={['OK']}
         />
       </IonContent>
       <IonFooter>
         <IonToolbar>
-          <div className="ion-margin">
+          <div className='ion-margin'>
             <IonButton
-              className="ion-margin"
-              expand="block"
-              shape="round"
+              className='ion-margin'
+              expand='block'
+              shape='round'
               onClick={() => {
                 handleUploadFile(avatarUrl);
               }}
--- src/utils/helpers/helpers.tsx
+++ src/utils/helpers/helpers.tsx
@@ -1,4 +1,5 @@
 import { storage } from '../../firebase';
+import jimp from 'jimp';
 
 export const isEmail = (email) => {
   const re =
@@ -14,4 +15,20 @@ export const deleteAllSubItemFirebase = (path: string) => {
     });
     Promise.all(promises);
   });
+};
+
+export const resizeImage = async (url: string, maxSize: number) => {
+  // Read the image.
+  const image = await jimp.read(url);
+
+  // Resize the image to width 150 and auto height.
+  console.log(image.getWidth(), image.getHeight());
+  const width = image.getWidth();
+  const height = image.getHeight();
+  if (width < height) {
+    image.resize(jimp.AUTO, maxSize);
+  } else image.resize(maxSize, jimp.AUTO);
+  // Save and overwrite the image
+  //await image.writeAsync("test/image.png");
+  return image.getBase64Async(jimp.MIME_JPEG);
 };
