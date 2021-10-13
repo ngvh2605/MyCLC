bb96bd754c78c515934ee847e8016d656a514d72
Viet Hoang Nguyen <ngvh2605@gmail.com>
2021-10-13 15:48:13 +1100

    Face detection, avatar verify

--- src/common/useUploadFile.tsx
+++ src/common/useUploadFile.tsx
@@ -3,7 +3,6 @@ import { database, storage } from "../firebase";
 import { deleteAllSubItemFirebase } from "../utils/helpers/helpers";
 
 const useUploadFile = (userId?: string) => {
-  const [progress, setProgress] = useState<number>(0);
   const [url, setUrl] = useState<string>("");
 
   const handleUploadImage = async (
@@ -21,54 +20,59 @@ const useUploadFile = (userId?: string) => {
     const snapshot = await storage.ref(`${folderPath}${imgName}`).put(image);
     const uploadedUrl = await snapshot.ref.getDownloadURL();
 
+    if (type === "avatar") imageFaceDetection(uploadedUrl);
     setUrl(uploadedUrl);
     return uploadedUrl;
   };
 
-  const handleUploadWithProgress = async (
-    blobUrl: any,
-    type?: "avatar" | "news" | "events"
-  ) => {
-    const imgName = `${Date.now()}`;
-    const rootFolder = `/${userId ? `users/${userId}` : "public"}`;
-    const folderPath = `${rootFolder}/${type ? type + "/" : ""}`;
-    if (blobUrl) {
-      if (type === "avatar") deleteAllSubItemFirebase(folderPath); //nếu là type avatar thì xóa hết thư mục avatar trước khi up ảnh
-      const response = await fetch(blobUrl);
-      const image = await response.blob();
-      const uploadTask = storage.ref(`${folderPath}${imgName}`).put(image);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
-          setProgress(progress);
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          storage
-            .ref(folderPath)
-            .child(imgName)
-            .getDownloadURL()
-            .then((url) => {
-              setUrl(url);
-              //save avatar url to database
-              if (type === "avatar") {
-                const userData = database.ref();
-                userData.child("users").child(userId).child("personal").update({
-                  avatar: url,
-                });
-              }
-            });
+  const imageFaceDetection = (url: string) => {
+    const options = {
+      method: "POST",
+      url: "https://face-detection6.p.rapidapi.com/img/face",
+      headers: {
+        "content-type": "application/json",
+        "x-rapidapi-host": "face-detection6.p.rapidapi.com",
+        "x-rapidapi-key": "215481281amsh1c8a8021452f9ecp149b18jsn2e71712e8c32",
+      },
+      data: {
+        url: url,
+        accuracy_boost: 1,
+      },
+    };
+
+    const axios = require("axios");
+
+    axios
+      .request(options)
+      .then(function (response: any) {
+        const data = response.data;
+        console.log(data);
+        if (
+          data.detected_faces &&
+          data.detected_faces.length &&
+          data.detected_faces.length === 1
+        ) {
+          const userData = database.ref();
+          userData.child("users").child(userId).child("verify").update({
+            avatarVerify: true,
+          });
+        } else {
+          const userData = database.ref();
+          userData.child("users").child(userId).child("verify").update({
+            avatarVerify: false,
+          });
         }
-      );
-    }
+      })
+      .catch(function (error: any) {
+        console.error(error);
+        const userData = database.ref();
+        userData.child("users").child(userId).child("verify").update({
+          avatarVerify: false,
+        });
+      });
   };
 
-  return { progress, url, handleUploadImage, handleUploadWithProgress };
+  return { url, handleUploadImage };
 };
 
 export default useUploadFile;
