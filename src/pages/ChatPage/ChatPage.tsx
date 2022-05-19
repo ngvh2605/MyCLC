79c9162bfdb2f23aa81561e386961f71580e9967
ngvh2605 <ngvh2605@gmail.com>
2022-05-19 23:53:39 +1000

    Chat

--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -1,6 +1,15 @@
-import { IonContent, IonPage } from "@ionic/react";
+import {
+  IonButtons,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import React, { useEffect, useState } from "react";
-import { StreamChat } from "stream-chat";
+import { DefaultGenerics, StreamChat } from "stream-chat";
 import {
   Chat,
   Channel,
@@ -9,53 +18,80 @@ import {
   MessageList,
   Thread,
   Window,
+  ChannelList,
 } from "stream-chat-react";
 import "stream-chat-react/dist/css/index.css";
 import "stream-chat-react/dist/css/index.css";
 import { useAuth } from "../../auth";
 import useCheckUserInfo from "../../common/useCheckUserInfo";
+import { auth } from "../../firebase";
+import "./ChatPage.scss";
 
 const api_key = "u6nz6buysmyr";
-
-const client = StreamChat.getInstance(api_key);
 
 const ChatPage: React.FC = () => {
   const { userId, userEmail } = useAuth();
   const { isVerify, avatarVerify, fullName, avatarUrl, allowCreateEvent } =
     useCheckUserInfo(userId);
 
-  const [channel, setChannel] = useState<any>();
+  const [client, setClient] = useState<StreamChat<DefaultGenerics>>(null);
+  const [channel, setChannel] = useState(null);
 
   useEffect(() => {
-    if (fullName) {
-      client
-        .connectUser(
-          {
-            id: userId,
-            name: fullName,
-            image: avatarUrl ? avatarUrl : "",
-          },
-          client.devToken(userId)
-        )
-        .then(async () => {
-          const temp = client.channel("messaging", "travel", {
-            name: "Awesome channel about traveling",
-          });
-          setChannel(temp);
-        });
+    async function init() {
+      const chatClient = StreamChat.getInstance(api_key);
+      await chatClient.connectUser(
+        {
+          id: userId,
+          name: auth.currentUser.displayName,
+          image: auth.currentUser.photoURL,
+        },
+        chatClient.devToken(userId)
+      );
+
+      const chatChannel = chatClient.channel("messaging", "travel", {
+        name: "Awesome channel about traveling",
+        members: [userId],
+      });
+
+      await chatChannel.watch();
+
+      setChannel(chatChannel);
+      setClient(chatClient);
     }
-  }, [fullName]);
+
+    if (userId) init();
+
+    return () => {
+      try {
+        client.disconnectUser();
+      } catch (err) {}
+    };
+  }, [userId]);
 
   return (
-    <IonPage>
-      <IonContent>
+    <IonPage id="chat-page">
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+
+          <IonTitle>Chat</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent scroll-y="false">
         {!!client && !!channel && (
-          <Chat client={client} theme="messaging light">
+          <Chat client={client} theme="messaging light" customStyles={{}}>
             <Channel channel={channel}>
               <Window>
-                <ChannelHeader />
-                <MessageList />
                 <MessageInput />
+                {/* <ChannelHeader
+                  MenuIcon={() => {
+                    return <></>;
+                  }}
+                /> */}
+                <MessageList />
               </Window>
               <Thread />
             </Channel>
