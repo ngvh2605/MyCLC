01e2c2cb14cadc6a5a1ba33fac3115bb88a72a69
Viet Hoang Nguyen <ngvh2605@gmail.com>
2022-05-30 16:53:32 +1000

    Certi Page UI

--- src/pages/CertificatePage/CertificatePage.tsx
+++ src/pages/CertificatePage/CertificatePage.tsx
@@ -1,10 +1,16 @@
 import {
   IonButtons,
   IonCard,
+  IonCardContent,
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
+  IonImg,
+  IonLabel,
   IonMenuButton,
   IonPage,
+  IonRow,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
@@ -22,7 +28,34 @@ const CertificatePage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonCard></IonCard>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="6">
+              <IonCard className="ion-no-margin" style={{ margin: 8 }}>
+                <IonImg src="https://scontent.fcbr1-1.fna.fbcdn.net/v/t39.30808-6/277738715_2137464263097110_7884089031801519933_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=e3f864&_nc_ohc=52GWwWZV9U8AX_DomdA&_nc_ht=scontent.fcbr1-1.fna&oh=00_AT_lP152CAKi_RVP_M5o_hL8TNXbeYwuUFZCvgUQOYflPQ&oe=6299D496" />
+                <IonCardContent>
+                  <IonLabel>Hello how are you 0</IonLabel>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="6">
+              <IonCard className="ion-no-margin" style={{ margin: 8 }}>
+                <IonImg src="https://scontent.fcbr1-1.fna.fbcdn.net/v/t39.30808-6/277738715_2137464263097110_7884089031801519933_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=e3f864&_nc_ohc=52GWwWZV9U8AX_DomdA&_nc_ht=scontent.fcbr1-1.fna&oh=00_AT_lP152CAKi_RVP_M5o_hL8TNXbeYwuUFZCvgUQOYflPQ&oe=6299D496" />
+                <IonCardContent>
+                  <IonLabel>Hello how are you 1</IonLabel>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+            <IonCol size="6">
+              <IonCard className="ion-no-margin" style={{ margin: 8 }}>
+                <IonImg src="https://scontent.fcbr1-1.fna.fbcdn.net/v/t39.30808-6/277738715_2137464263097110_7884089031801519933_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=e3f864&_nc_ohc=52GWwWZV9U8AX_DomdA&_nc_ht=scontent.fcbr1-1.fna&oh=00_AT_lP152CAKi_RVP_M5o_hL8TNXbeYwuUFZCvgUQOYflPQ&oe=6299D496" />
+                <IonCardContent>
+                  <IonLabel>Hello how are you 2</IonLabel>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
